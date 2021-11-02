d9d05472d65679bac289234e9ae1d2047eed8002
Jianxian <30934916+m839336369@users.noreply.github.com>
2021-11-02 17:15:35 +0800

    1.Annotation分离出类和方法 2.增设服务和请求类的Init\UnInit 3.将NetNode分离，为后续Plugin开发做准备

--- src/main/java/com/ethereal/server/Net/Abstract/NetConfig.java
+++ src/main/java/com/ethereal/server/Net/Abstract/NetConfig.java
@@ -1,10 +1,5 @@
 package com.ethereal.server.Net.Abstract;
 
-import com.ethereal.client.Client.Abstract.ClientConfig;
-import org.javatuples.Pair;
-
-import java.util.List;
-
 /**
  * @ProjectName: YiXian_Client
  * @Package: com.yixian.material.RPC
@@ -18,40 +13,5 @@ import java.util.List;
  * @Version: 1.0
  */
 public abstract class NetConfig {
-    /// <summary>
-    /// 分布式模式是否开启
-    /// </summary>
-    protected Boolean netNodeMode = false;
-    /// <summary>
-    /// 分布式IP组
-    /// </summary>
-    protected List<Pair<String, ClientConfig>> netNodeIps;
-    /// <summary>
-    /// 服务注册心跳间隔
-    /// </summary>
-    protected int netNodeHeartInterval = 20000;
 
-    public Boolean getNetNodeMode() {
-        return netNodeMode;
-    }
-
-    public void setNetNodeMode(Boolean netNodeMode) {
-        this.netNodeMode = netNodeMode;
-    }
-
-    public List<Pair<String, ClientConfig>> getNetNodeIps() {
-        return netNodeIps;
-    }
-
-    public void setNetNodeIps(List<Pair<String, ClientConfig>> netNodeIps) {
-        this.netNodeIps = netNodeIps;
-    }
-
-    public int getNetNodeHeartInterval() {
-        return netNodeHeartInterval;
-    }
-
-    public void setNetNodeHeartInterval(int netNodeHeartInterval) {
-        this.netNodeHeartInterval = netNodeHeartInterval;
-    }
 }
